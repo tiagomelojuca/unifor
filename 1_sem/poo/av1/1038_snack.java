7790d3dbdb98d2ad7f5508558dc5f66308e74b42
Tiago Melo Juca <contact@tiagomelojuca.com>
2022-06-14 16:20:52 -0300

    fix wrong submitions

--- 1_sem/poo/av1/1038_snack.java
+++ 1_sem/poo/av1/1038_snack.java
@@ -1,72 +1,66 @@
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
-interface Snack { public float GetPrice(); }
-class CachorroQuente implements Snack { public float GetPrice() { return 4.00f; } }
-class XSalada        implements Snack { public float GetPrice() { return 4.50f; } }
-class XBacon         implements Snack { public float GetPrice() { return 5.00f; } }
-class TorradaSimples implements Snack { public float GetPrice() { return 2.00f; } }
-class Refrigerante   implements Snack { public float GetPrice() { return 1.50f; } }
-
-class SnackFactory {
-    int code;
+class Snack {
+    String spec;
+    int price;
     
-    SnackFactory(int _code) {
-        code = _code;
+    Snack(String _spec, int _price) {
+        spec  = _spec;
+        price = _price;
     }
     
-    public Snack Create() {
-        switch (code) {
-            case 1:  return new CachorroQuente();
-            case 2:  return new XSalada();
-            case 3:  return new XBacon();
-            case 4:  return new TorradaSimples();
-            case 5:  return new Refrigerante();
-            default: return null;
-        }
+    public String GetSpecification() { return spec; }
+    public int GetPrice() { return price; }
+}
+
+class SnacksMenu {
+    Map<Integer, Snack> snacks = new HashMap<Integer, Snack>();
+    
+    SnacksMenu() {
+        snacks.put(1, new Snack("Cachorro Quente", 400));
+        snacks.put(2, new Snack("X-Salada", 450));
+        snacks.put(3, new Snack("X-Bacon", 500));
+        snacks.put(4, new Snack("Torrada Simples", 200));
+        snacks.put(5, new Snack("Refrigerante", 150));
     }
+    
+    public Snack GetSnack(int code) { return snacks.get(code); }
 }
 
-class Pair {
-    int elem1; int elem2;
-    Pair(int _elem1, int _elem2) { elem1 = _elem1; elem2 = _elem2; }
-    public int getKey()          { return elem1; }
-    public int getValue()        { return elem2; }
-}
-
-class Calculator {
-    private Pair ReadFromKeyboard() {
+class Request {
+    private SnacksMenu menu;
+    private int total;
+    
+    Request (SnacksMenu _menu) {
+        menu = _menu;
+    }
+    
+    public void readRequest() {
         Scanner in = new Scanner(System.in);
-        int cod = in.nextInt();
-        int qtd = in.nextInt();
-        return new Pair(cod, qtd);
-    }
-    
-    float CalcPrice(int cod, int qtd) {
-        SnackFactory factory = new SnackFactory(cod);
-        Snack snack = factory.Create();
         
-        if (snack == null) {
-            return -1.0f;
-        }
+        int x = in.nextInt();
+        int y = in.nextInt();
         
-        return snack.GetPrice() * qtd;
+        total = menu.GetSnack(x).GetPrice() * y;
+        
+        in.close();
     }
     
-    float CalcPrice(Pair codQtd) {
-        return CalcPrice(codQtd.getKey(), codQtd.getValue());
-    }
-    
-    public void PrintToScreen() {
-        System.out.printf("Total: R$ %.2f\n", CalcPrice(ReadFromKeyboard()));
+    public void printTotal() {
+        System.out.printf("Total: R$ %.2f\n", (float) total / 100);
     }
 }
-
+ 
 public class Main {
  
     public static void main(String[] args) throws IOException {
-        Calculator c = new Calculator();
-        c.PrintToScreen();
+        Request request = new Request(new SnacksMenu());
+        
+        request.readRequest();
+        request.printTotal();
     }
  
 }
